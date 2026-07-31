d8c86f823501d94d7d87faace109171e5295c507
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-07-31 12:06:38 +0300

    Fri Jul 31 12:06:38 EEST 2026

--- utest.sh
+++ utest.sh
@@ -1,5 +1,5 @@
-python -m venv venv && source venv/bin/activate
+python3 -m venv venv && source venv/bin/activate
 pip install -r requirements.txt
 # only on Apple Silicon Macs, optionally:
 #pip install -r requirements-mlx.txt
-python app.py
+python3 app.py
